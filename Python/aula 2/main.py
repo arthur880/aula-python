617720d266de6db48bf8c959494c7cebd77b9d91
arthur880 <cleitineshalin@gmail.com>
2024-09-16 17:55:18 -0300

    Add files via upload

---
+++ Python/aula 2/main.py
@@ -0,0 +1,6 @@
+number1 = int(input("digite sua idade: "))
+number2 =int(input("digite sua idade "))
+if number1 < number2: 
+    print(f"o numero {number1}e menor que o {number2}")
+elif number1 > number2:
+    print(f"o numero {number1} e maior que o numero {number2}")
